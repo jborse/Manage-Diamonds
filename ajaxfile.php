cd768f214474bda1e7399cb313a7fc7ebae627d5
jborse <39696966+jborse@users.noreply.github.com>
2019-04-08 16:48:14 +0530

    Update ajaxfile.php

--- ajaxfile.php
+++ ajaxfile.php
@@ -23,17 +23,12 @@ $totalRecords = count($sel);
 
 
 ## Total number of records with filtering
-/* $sel = mysqli_query($con,"select count(*) as allcount from employee WHERE 1 ".$searchQuery);
-$records = mysqli_fetch_assoc($sel); */
-
 $sel = $wpdb->get_results('SELECT * FROM ' . $wpdb->prefix . 'diamond_stock where 1 '.$searchQuery);
 $totalRecordwithFilter = count($sel);
 
 
 ## Fetch records
-//$empQuery = "select * from employee WHERE 1 ".$searchQuery." order by ".$columnName." ".$columnSortOrder." limit ".$row.",".$rowperpage;
 $empRecords = $wpdb->get_results('SELECT * FROM ' . $wpdb->prefix . 'diamond_stock where 1 '.$searchQuery.' order by ' . $columnName . ' ' . $columnSortOrder . ' limit ' . $row . ',' . $rowperpage);
-/* $empRecords = mysqli_query($con, $empQuery); */
 $data = array();
 
 foreach ($empRecords as $row) {
@@ -71,69 +66,5 @@ $response = array(
 );
 
 echo json_encode($response);
-
 exit();
-
-
-/* global $wpdb;
-
-$results['draw'] = $_POST['draw'];
-$search=trim($_POST['search']['value']);
-if($search!=""){
-    $jde_refd=" reportNo like '%".$search."%' AND reportNo !=''";
-}else{
-    $jde_refd="1=1 AND reportNo !=''";
-}
-
-$resultCount = $wpdb->get_results('SELECT * FROM ' . $wpdb->prefix . 'diamond_stock where '.$jde_refd);
-
-//  
-$length = $_POST['length'];
-$start = $_POST['start'];
-$arrayField = array(
-    'reportNo',
-    'reference',
-    'shape',
-    'lab',
-    'weight',
-    'color',
-    'clarity',
-    'cut',
-    'polish',
-    'symmetry',
-    'fluorescence',
-    'measurement',
-    'depth',
-    'table',
-    'girdle',
-    'priceCarat',
-    'price',
-    'available',
-    'certificateLink',
-    'videoLink'
-);
-
-$results['recordsTotal'] = count($resultCount);
-$results['recordsFiltered'] = count($resultCount);
-$results['data']="";
-if ($resultCount > 0) {
-    $row = $wpdb->get_results('SELECT * FROM ' . $wpdb->prefix . 'diamond_stock where '.$jde_refd.' order by ' . $arrayField[$_POST['order'][0]['column']] . ' ' . $_POST['order'][0]['dir'] . ' limit ' . $start . ',' . $length);
-    $i = 0;
-    //$statusArray = array("No", "Yes");
-    foreach ($row as $r) {
-		
-		$results['data'][$i] = $r;
-        //echo number_format("1000000",2,",",".");
-        $results['data'][$i]->price=number_format($results['data'][$i]->price,0);        
-        $results['data'][$i]->priceCarat=number_format($results['data'][$i]->priceCarat,0);        
-       // $results['data'][$i]->shape= ucfirst($results['data'][$i]->shape);
-       // $results['data'][$i]->depth= number_format($results['data'][$i]->depth,1).'%';
-        $results['data'][$i]->available = "<span class='status' data-id='" . $r->ds_id . "' data-status='" . $r->available . "'>" . $r->available . "</span>";
-        //$results['data'][$i]->action = "<span title='Remove Diamond' data-id='" . $r->diamond_id . "' class='dashicons-before action-del wp-menu-image dashicons-trash'></span>";
-        //$results['data'][$i]->action = "-";
-        $i++;
-    }
-}
-echo json_encode($results);
-die(); */
 ?>
